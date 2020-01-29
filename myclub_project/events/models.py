3b4da314f6d32fd899d79f360b6f476aa9309762
jaebooker <jaesonbooker@gmail.com>
2020-01-29 14:57:04 -0800

    changes

--- myclub_project/events/models.py
+++ myclub_project/events/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_date = models.DateTimeField('Event Date')
-    venue = models.CharField(max_length=120)
-    manager = models.CharField(max_length = 60)
+    venue = models.CharField(max_length=130)
+    manager = models.CharField(max_length=100)
     description = models.TextField(blank=True)
