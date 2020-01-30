64fa0e52318c246f00ed08a374abf5d9ad23a918
jaebooker <jaesonbooker@gmail.com>
2020-01-29 18:37:30 -0800

    fixed migrations

--- myclub_project/events/models.py
+++ myclub_project/events/models.py
@@ -25,11 +25,11 @@ class Venue(models.Model):
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_date = models.DateTimeField('Event Date')
-    venue = models.CharField(max_length=130)
+    #venue = models.CharField(max_length=130)
     manager = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(MyclubUser)
-    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    venue = models.ForeignKey(Venue, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
