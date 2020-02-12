2ffcfac67f642ff4cde9e3bd088516f9ce640908
jaebooker <jaesonbooker@gmail.com>
2020-02-11 16:21:01 -0800

    django tutorial

--- dJango/polls/urls.py
+++ dJango/polls/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('now', views.show_the_time),
     path('hello', views.SayHello.as_view()),
+    path('<int:question_id>/results', views.results, name='results'),
+    path('<int:question_id>/vote', views.vote, name='vote'),
 ]
