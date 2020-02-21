09323d6523977110898362f4e32353d6c0d22504
jaebooker <jaesonbooker@gmail.com>
2020-02-21 12:08:39 -0800

    added other things

--- dJango/polls/views.py
+++ dJango/polls/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.views import View
 from django.template import loader
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     return HttpResponse("Good Morning, Starshine! The Earth says Hello!")
@@ -34,7 +34,18 @@ def question_detail(request, question_id):
     return render(request, 'polls/detail.html',{'question':question})
 
 def vote(request, question_id):
-    return HttpResponse("You've cast your vote for %s!" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a correct choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
 
 def results(request, question_id):
     response = "The answer is... %s!"
