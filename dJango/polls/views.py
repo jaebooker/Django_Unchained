ddd84b668069938cc97544e0f6f1408f3c037670
jaebooker <jaesonbooker@gmail.com>
2020-02-22 18:16:48 -0800

    additions

--- dJango/polls/views.py
+++ dJango/polls/views.py
@@ -48,8 +48,10 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
 
 def results(request, question_id):
-    response = "The answer is... %s!"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
+    # response = "The answer is... %s!"
+    # return HttpResponse(response % question_id)
 
 class SayHello(View):
 
