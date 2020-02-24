c433828985b71ff438e39c5d6bafd0d753438e6c
jaebooker <jaesonbooker@gmail.com>
2020-02-23 18:41:09 -0800

    additions

--- dJango/polls/views.py
+++ dJango/polls/views.py
@@ -6,6 +6,21 @@ from django.template import loader
 
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 def index(request):
     return HttpResponse("Good Morning, Starshine! The Earth says Hello!")
 
